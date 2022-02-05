f91dfc4b2905fdf3c534ff24e12b8292fb7ba246
yeonlisa <tkzw4869@gmail.com>
2022-02-05 14:49:23 +0900

    Understanding State

--- react.js
+++ react.js
@@ -1,42 +1,16 @@
 const root = document.getElementById("root");
-const Title = () => (
-    <h3 id="title" onMouseEnter={() => console.log("mouse enter")}>
-        Hello I'm a title
-    </h3>
-);
-// const h3 = React.createElement(
-//     "h3", 
-//     {
-//         id: "title",
-//         onMouseEnter: () => console.log('mouse enter'),
-//     }, 
-//     "Hello I'm a title"
-// );
-const Button = () => (
-    <button 
-        style={{
-            backgroundColor: "tomato",
-        }} 
-        onClick={() => console.log("im clicked")}
-    >
-        Click me
-    </button>
-);
-// const btn = React.createElement(
-//     "button", 
-//     {
-//         onClick: () => console.log('im clicked'),
-//         style: {
-//             backgroundColor: "tomato",
-//         },
-//     }, 
-//     "Click me"
-// );
-const container = (
+let counter = 0;
+const countUp = () => {
+    counter = counter + 1;
+    render()
+}
+const render = () => {
+    ReactDOM.render(<Container />, root);
+}
+const Container = () => (
     <div>
-        <Title /> 
-        <Button />
+        <h3>Total clicks: {counter}</h3>
+        <button onClick={countUp}>Click me</button>
     </div>
 );
-// const container = React.createElement("div", null, [h3, btn])
-ReactDOM.render(container, root);
+render();
