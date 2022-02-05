01d985359b9230c84e4897bff0b9de843880b19b
yeonlisa <tkzw4869@gmail.com>
2022-02-05 13:44:07 +0900

    Our First React Element

---
+++ react.js
@@ -0,0 +1,7 @@
+const root = document.getElementById("root");
+const span = React.createElement(
+    "span", 
+    { id: "sexy-span", style: { color: "red" } }, 
+    "Hello I'm a span"
+);
+ReactDOM.render(span, root)
